a7c9b53cfb491e61c969b23e5be55c010b6e6927
zhangqing <zhangqing332@live.com>
2018-11-26 10:19:08 +0800

    BREAKING CHANGE: The graphiteWidth option has been removed. The default graphite width of 10mm is always used for performance reasons.
    * #1

--- index.js
+++ index.js
@@ -17,8 +17,18 @@ console.log("chore: Changes to the build process or auxiliary tools and librarie
 console.log("perf: C2hanges to the build process or auxiliary tools and libraries such as documentation generation")
 
 
-function fixRealse() {
-  console.log("fix: 运行`fix`指令可以发动小修改")
+function fixRelease() {
+  console.log("fix: 运行`fix`指令可以发动小修改 1.0.1 -> 1.0.2")
 
-  console.log("perf: 运行`perf`指令可以发动小修改")
+  console.log("perf: 运行`perf`指令可以发动小修改 1.0.1 -> 1.0.2")
+  
+}
+
+function featureRelaase() {
+  console.log("feat(pencil): 这回导致一个特性发布 1.1.1 -> 1.2.0")
+}
+
+function breadChangeRelease() {
+  console.log("BREAKING CHANGE: The graphiteWidth option has been removed.\n" +
+    "The default graphite width of 10mm is always used for performance reasons. 1.2.0 -> 2.0.0")
 }
